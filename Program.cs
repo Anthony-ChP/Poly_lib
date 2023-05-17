49d8dcbbca3c36e96ee7081113dbdfc9c130b400
Anthony-ChP <darth.filth@gmail.com>
2023-05-17 19:11:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Poly_dll;
+
+DPolygon dacha = new DPolygon();
+
+dacha.DAddVertex(885.92, 285.23);
+dacha.DAddVertex(905.23, 289.76);
+dacha.DAddVertex(884.55, 330.37);
+dacha.DAddVertex(867.07, 329.62);
+
+dacha.DPolygonPrint();
+Console.WriteLine($"Perimeter: {dacha.DPerimeter()}");
+Console.WriteLine($"Square: {dacha.DSquare()}");
